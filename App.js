49ef10d6f99d00be3069e1f2496140ae1003ddec
colin.tenaguillo@ynov.com <colin.tenaguillo@ynov.com>
2020-01-15 13:38:23 +0100

    FooterMenu

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import ButtonG from "./src/components/button";
-import SlotItem from "./src/components/slotItem/Test";
+import { AppLoading } from "expo";
+import { Container, Text } from "native-base";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import FooterMenu from "./src/components/footer";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
     return (
-      <View style={styles.container}>
-        <SlotItem></SlotItem>
-      </View>
+      <Container>
+        <FooterMenu></FooterMenu>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
